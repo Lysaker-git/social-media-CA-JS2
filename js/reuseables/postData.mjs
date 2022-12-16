b0856c200384411bee9cf7122261d097a2af3d85
Lysaker-git <68820814+Lysaker-git@users.noreply.github.com>
2022-12-16 15:11:30 +0100

    postdata JSdocs and userData JSdocs

--- js/reuseables/postData.mjs
+++ js/reuseables/postData.mjs
@@ -1,3 +1,10 @@
+/**
+ * Getting passed title and body of post and builds a postData to pass to API. 
+ * @param {string} title Title of the post
+ * @param {string} body Body or content of the post
+ * @returns post variable that is OK for API
+ */
+
 export function postContent(title, body) {
     return post = {
         title: postTitle,
--- js/reuseables/userData.mjs
+++ js/reuseables/userData.mjs
@@ -1,3 +1,11 @@
+/**
+ * 
+ * @param {string} mail - mail in string format for object
+ * @param {string} pass - pass as string format for object
+ * @param {string} [user] - OPTIONAL user as string format for object
+ * @returns user variable to pass to API. 
+ */
+
 export function userData(mail, pass, user = false) {
     if (user) {
         return user = {
