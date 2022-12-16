0dcf3df469118497224df299b3f6be115b83e15d
Lysaker-git <68820814+Lysaker-git@users.noreply.github.com>
2022-12-16 15:06:38 +0100

    Profile JSDocs added

--- js/profile/profile.mjs
+++ js/profile/profile.mjs
@@ -1,6 +1,12 @@
 import { headerWithAuth } from "../api/headers.mjs";
 import { profileTemplate } from "../templates/index.mjs";
 
+
+/**
+ * Function to fetch profile of logged in user. Passes to function profileData(data, document)
+ * This is to access document element inside the creation of profileHTML - I wanted to try different aproaches to creating HTML. 
+ * @param {string} url 
+ */
 export async function fetchProfile (url) {
     try {
         const name = localStorage.getItem('name');
@@ -13,7 +19,12 @@ export async function fetchProfile (url) {
         console.log(e);
     }
 }
- 
+
+/**
+ * Getting passed profile and document to create the HTML for profile. Passes created elements with profile to the profileTemplate then appends it on the wrapper parent. 
+ * @param {object} profile User-profile object
+ * @param {document} doc document so i can access document.createElement and document.querySelector
+ */
 function profileData (profile, doc) {
 
     const profileContainer = doc.querySelector('.profileContainer')
